453d1d1ba7e6e8a3eb7b22dd9a47fa17df49b92d
Per Lundholm <per@artcomputer.se>
2018-10-07 18:58:49 +0200

    Server starts with a JavaFX GUI that has a big stop button.

--- server/src/main/java/se/artcomputer/desktopweb/Server.java
+++ server/src/main/java/se/artcomputer/desktopweb/Server.java
@@ -2,6 +2,12 @@ package se.artcomputer.desktopweb;
 
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,22 +17,40 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 
-public class Server {
+public class Server extends Application {
 
     private static final Logger LOG = LogManager.getLogger();
 
     private static final int PORT = 8080;
 
+    private String message = "The server started at http://localhost:" + PORT;
+
     public static void main(String[] args) {
+        launch(args);
+    }
 
+    @Override
+    public void init() {
         long startTime = System.currentTimeMillis();
-
         try {
             new Server().run();
         } catch (IOException e) {
             LOG.log(Level.ERROR, "Problem starting", e);
+            message = "That did not go well: " + e.getMessage();
         }
         LOG.info(String.format("Server started on port %d in %d ms", PORT, (System.currentTimeMillis() - startTime)));
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button stopButton = new Button("Stop");
+        stopButton.setStyle("-fx-font: 22 arial; -fx-base: #b60000;");
+        stopButton.setOnAction(event -> System.exit(0));
+        Label label = new Label(message);
+        VBox vBox = new VBox(label, stopButton);
+        primaryStage.setScene(new Scene(vBox, 300, 250));
+        primaryStage.setTitle("Desktop web server");
+        primaryStage.show();
     }
 
     private void run() throws IOException {
